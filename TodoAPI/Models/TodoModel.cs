ad83342cf8acee2dc232d8cfea1059a501c6420a
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-02-22 11:45:38 +0100

    Add project files.

---
+++ TodoAPI/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoAPI.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+    }
+}
+
+
